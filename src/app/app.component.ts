0636019128a81be284d64d636042824758e330d8
Arkniid <n.hallouin@gmail.com>
2017-05-14 17:05:48 +0200

    First commit for sandbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit, AfterViewInit {
+    title = 'ZColoc still in tests';
+
+    ngOnInit(): void {
+      console.log("ngOnInit");
+    }
+
+    ngAfterViewInit(): void {
+      $("#sideMenu").sideNav();
+    }  
 }
